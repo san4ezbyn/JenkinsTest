5df48ec51cb5b1ec08549e0325d84fd935bbd1d0
san4ezbyn <san4ezbyn@gmail.com>
2018-07-23 14:36:38 +0300

    Init

---
+++ src/test/java/packmvn/MavenClass.java
@@ -0,0 +1,37 @@
+package packmvn;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class MavenClass {
+
+    @Test
+    public void simpleTest(){
+        int a = 1;
+        int b = 2;
+        int c =3;
+        System.out.println("Hello");
+        assertTrue (a+b == c);
+
+    }
+
+    @Test
+    public void simpleTestOK(){
+        int a = 1;
+        int b = 3;
+        int d =4;
+        System.out.println("World");
+        assertTrue (a+b == d);
+    }
+
+    @Test
+    public void simpleTestFail(){
+        int a = 1;
+        int b = 9;
+
+        System.out.println("Hang on there!");
+        assertTrue (a+b == 100);
+    }
+
+}
