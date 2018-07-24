e056461d60635725b88e1b02998c9e6bdf4622d2
san4ezbyn <san4ezbyn@gmail.com>
2018-07-24 13:27:09 +0300

    POsitive

--- src/test/java/packmvn/MavenClass.java
+++ src/test/java/packmvn/MavenClass.java
@@ -10,7 +10,7 @@ public class MavenClass {
 
     @org.testng.annotations.Test
     public void simpleTest(){
-        int a = 10;
+        int a = 1;
         int b = 2;
         int c =3;
         System.out.println("Hello");
@@ -20,7 +20,7 @@ public class MavenClass {
 
     @org.testng.annotations.Test
     public void simpleTestOK(){
-        int a = 10;
+        int a = 1;
         int b = 3;
         int d =4;
         System.out.println("World");
@@ -33,7 +33,7 @@ public class MavenClass {
         int b = 9;
 
         System.out.println("Hang on there!");
-        Assert.assertTrue (a+b == 100);
+        Assert.assertTrue (a+b == 10);
     }
 
    @org.testng.annotations.Test
@@ -42,7 +42,7 @@ public class MavenClass {
         int b = 9;
 
         System.out.println("It's gonna be OK! SURE!!!");
-        Assert.assertTrue (a>b );
+        Assert.assertTrue (a<b );
     }
 
 }
