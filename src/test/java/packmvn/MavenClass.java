98c5dce198baff6ffbafeba0111f733a1ac46906
san4ezbyn <san4ezbyn@gmail.com>
2018-07-24 10:54:17 +0300

    Trjam:))

--- src/test/java/packmvn/MavenClass.java
+++ src/test/java/packmvn/MavenClass.java
@@ -1,46 +1,48 @@
 package packmvn;
+import org.testng.TestNG;
 
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.xml.dom.Tag;
 
 import static junit.framework.TestCase.assertTrue;
 
 public class MavenClass {
 
-    @Test
+    @org.testng.annotations.Test
     public void simpleTest(){
         int a = 1;
         int b = 2;
         int c =3;
         System.out.println("Hello");
-        assertTrue (a+b == c);
+        Assert.assertTrue (a+b == c);
 
     }
 
-    @Test
+    @org.testng.annotations.Test
     public void simpleTestOK(){
         int a = 1;
         int b = 3;
         int d =4;
         System.out.println("World");
-        assertTrue (a+b == d);
+        Assert.assertTrue (a+b == d);
     }
 
-    @Test
+   @org.testng.annotations.Test
     public void simpleTestFail(){
         int a = 1;
         int b = 9;
 
         System.out.println("Hang on there!");
-        assertTrue (a+b == 100);
+        Assert.assertTrue (a+b == 100);
     }
 
-    @Test
+   @org.testng.annotations.Test
     public void simpleTestSup(){
         int a = 1;
         int b = 9;
 
-        System.out.println("It's gonna be OK!");
-        assertTrue (a<b );
+        System.out.println("It's gonna be OK! SURE!!!");
+        Assert.assertTrue (a<b );
     }
 
 }
